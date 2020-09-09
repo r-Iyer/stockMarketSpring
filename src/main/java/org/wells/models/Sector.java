b97b8e1ac8c9f7fc353c35d7ffbbab307febfdce
Rohit Iyer <rohitstar.iyer@gmail.com>
2020-09-09 13:14:02 +0530

    Changed lazy to eager

--- src/main/java/org/wells/models/Sector.java
+++ src/main/java/org/wells/models/Sector.java
@@ -18,7 +18,7 @@ public class Sector {
     @Column(nullable = false)
     private String sectorName;
     private String brief;
-    @OneToMany(mappedBy = "sector", fetch = FetchType.LAZY) //changed
+    @OneToMany(mappedBy = "sector", fetch = FetchType.LAZY) //changed here
     @JsonIgnore
     private List<Company> companies;
 
